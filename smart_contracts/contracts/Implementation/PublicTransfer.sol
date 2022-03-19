4f02b338486efafc0c7e31b4421ff38bcfb28d68
joulesler <gzxce@hotmail.com>
2022-03-19 15:42:05 +0800

    Initial commit for transfers

---
+++ smart_contracts/contracts/Implementation/PublicTransfer.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "../Libraries/LibTransfer.sol";
+
+contract PublicTransfer{
+    function directTransfer() public returns (){
+
+    }
+}
---
+++ smart_contracts/contracts/Libraries/LibTransfer.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+library LibTransfer {
+
+    struct CreditTsfInst{
+        uint decimals;
+        bytes4 ccy;
+        uint amount;
+    }
+
+}
