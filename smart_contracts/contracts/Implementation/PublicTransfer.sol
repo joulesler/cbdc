9d770097baccc44bbf32ba8dd81574cd1ab9c585
joulesler <gzxce@hotmail.com>
2023-03-25 13:57:13 +0800

    Sync progress

--- smart_contracts/contracts/Implementation/PublicTransfer.sol
+++ smart_contracts/contracts/Implementation/PublicTransfer.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "../Libraries/LibTransfer.sol";
 
 contract PublicTransfer{
-    function directTransfer() public returns (){
-
+    function directTransfer() public returns (LibTransfer.CreditTsfInst memory trsf){
+        
     }
 }
